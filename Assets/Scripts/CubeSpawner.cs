28348eb82996e7b87919bed3cb4bc12eaba6f6bb
LincThai <78945845+LincThai@users.noreply.github.com>
2024-08-03 12:14:42 +1000

    Current  (Teacher Changes & Assignment Package)

---
+++ Assets/Scripts/CubeSpawner.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class CubeSpawner : MonoBehaviour
+{
+    public static CubeSpawner Instance {  get; private set; }
+
+    [SerializeField] private SpawnedCube cubePrefab;
+    [SerializeField] private Transform origin;
+
+    private ObjectPool<SpawnedCube> cubePool;
+    public ObjectPool<SpawnedCube> CubePool => cubePool;
+
+    private int spawnRate = 16;
+    private int maxSpawns = 10000;
+    [HideInInspector] public int spawnedCubes;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private async void Start()
+	{
+        CreatePools();
+
+        Task spawnCubesTask = SpawnCubes();
+        await spawnCubesTask;
+
+        if (spawnCubesTask.IsFaulted || spawnCubesTask.IsCanceled)
+        {
+            Debug.LogException(spawnCubesTask.Exception);
+        }
+	}
+    
+    private void CreatePools()
+    {
+        cubePool = new ObjectPool<SpawnedCube>(
+            CreateCube,
+            ActivateCube,
+            DeactivateCube,
+            DestroyCube,
+            false,
+            0, maxSpawns);
+    }
+
+    private SpawnedCube CreateCube() 
+    {
+        SpawnedCube cube = Instantiate(cubePrefab, origin);
+        return cube;
+    }
+
+    private void ActivateCube(SpawnedCube cube)
+    {
+        cube.gameObject.SetActive(true);
+    }
+
+    private void DeactivateCube(SpawnedCube cube)
+    {
+        cube.gameObject.SetActive(false); 
+    }
+
+    private void DestroyCube(SpawnedCube cube)
+    {
+        Destroy(cube.gameObject);
+    }
+
+    private async Task SpawnCubes()
+    {
+        while (cubePool.CountActive < maxSpawns)
+        {
+            SpawnCube();
+            await Task.Delay(spawnRate);
+        }
+    }
+
+    private void SpawnCube() 
+    {
+        cubePool.Get(out SpawnedCube newSpawnedCube);
+
+        if (!newSpawnedCube) return;
+        newSpawnedCube.Initialized();
+    }
+
+
+	//private IEnumerator SpawnCubes()
+    //{
+        //spawnedCubes = 0;
+
+        //while (spawnedCubes < maxSpawns)
+        //{
+		//	int spawnAmount = 10;
+        
+		//	for (int i = 0; i < spawnAmount; i++)
+            //{      
+              //  SpawnedCube cube = Instantiate(cubePrefab, origin);
+                //cube.GetComponent<SpawnedCube>().SetRandomVelocity();
+            //}
+
+			//yield return new WaitForSeconds(spawnRate);
+		//}
+    //}
+}
---
+++ Assets/Scripts/SpawnedCube.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Collections;
+
+public class SpawnedCube : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rb;
+
+    [SerializeField] private Vector2 xVelocityRange = new(-1, 1);
+    [SerializeField] private float verticalVelocity = 10;
+    [SerializeField] private Vector2 zVelocityRange = new(-1, 1);
+
+    [SerializeField] private int lifetime = 10000;
+
+    private Coroutine lifetimeCoroutine;
+    private WaitForSeconds lifetimeWait;
+
+    private void awake() 
+    {
+        lifetimeWait = new WaitForSeconds(lifetime);
+    }
+
+    private void OnEnabled()
+    {
+        lifetimeCoroutine = StartCoroutine(SetLifetime());
+    }
+
+    private void OnDisabled()
+    {
+        if (lifetimeCoroutine != null)
+            StopCoroutine(lifetimeCoroutine);
+    }
+
+    private IEnumerator SetLifetime()
+    {
+        yield return lifetimeWait;
+
+        if (gameObject != null && gameObject.activeSelf)
+            CubeSpawner.Instance.CubePool.Release(this);
+    }
+
+    public void Initialized()
+    {
+        SetRandomVelocity();
+    }
+
+    public void SetRandomVelocity()
+    {
+
+		rb.velocity = new Vector3(
+            Random.Range(xVelocityRange.x, xVelocityRange.y),
+            verticalVelocity, 
+            Random.Range(zVelocityRange.x, zVelocityRange.y));
+
+        rb.angularVelocity = new Vector3(
+            Random.Range(-10, 10), 
+            Random.Range(-10, 10), 
+            Random.Range(-10, 10));
+    }
+}
